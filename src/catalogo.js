c66e37c6db0e85c4e48095a2e6acbb1996cd0108
luky810la <Lukyzabala@gmail.com>
2025-12-01 20:18:15 -0400

    Update catalogo.js

--- src/catalogo.js
+++ src/catalogo.js
@@ -1,3 +1,4 @@
 // Registro de usuarios – Desarrollador B
 console.log("Catálogo actualizado por el Desarrollador B.");
+console.log("Segundo cambio realizado por el Desarrollador B.");
 console.log("Tercer cambio realizado por el Desarrollador B.");
